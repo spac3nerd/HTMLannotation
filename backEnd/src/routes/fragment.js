c169de9d9f331eb7d3f56f4fdb5a7253ae3cf5bd
Sorin Badila <sorin.badila@yahoo.com>
2017-12-16 15:50:35 -0500

    cleanup

--- backEnd/src/routes/fragment.js
+++ backEnd/src/routes/fragment.js
@@ -32,47 +32,13 @@ function send200Res(packet, res) {
 };
 
 router.post("/fragment/addAnnotation", function(req, res) {
-	//var packet = {};
 	var packet = getModelPacket(fragmentModel.addAnnotation(req.body.fragId, req.body.ann));
-	/*
-	var modelRes = fragmentModel.addAnnotation(req.body.fragId, req.body.ann);
-	if (modelRes != false) {
-		packet = {
-			success: true,
-			data: modelRes
-		}
-	}
-	else {
-		packet = {
-			success: false
-		}
-	}
-	*/
 	send200Res(packet, res);
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.end(JSON.stringify(packet), "utf-8");
 });
 
 router.post("/fragment/deleteAnnotation", function(req, res) {
-	//var packet = {};
 	var packet = getModelPacket(fragmentModel.deleteAnnotation(req.body.fragId, req.body.selId));
-	//var modelRes = fragmentModel.deleteAnnotation(req.body.fragId, req.body.selId);
-	/*
-	if (modelRes != false) {
-		packet = {
-			success: true,
-			data: modelRes
-		}
-	}
-	else {
-		packet = {
-			success: false
-		}
-	}
-	*/
 	send200Res(packet, res);
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.end(JSON.stringify(packet), "utf-8");
 });
 
 module.exports = router;
